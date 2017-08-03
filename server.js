ea38f1ed2c03e64ac353e2a183dedc6d98f07af6
morgan greenwalt <you@example.com>
2017-08-03 14:59:31 -0500

    edits

--- server.js
+++ server.js
@@ -6,11 +6,16 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var db = require("./models");
+var exphbs = require("express-handlebars");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -23,14 +28,14 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-// require("./app/routes/api-routes.js")(app);
-// require("./app/routes/html-routes.js")(app);
+require("./migrations/api-routes.js")(app);
+require("./migrations/html-routes.js")(app);
 
-require("./migrations/api-routes.js");
-require("./migrations/html-routes.js");
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });
